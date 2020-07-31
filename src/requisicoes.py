b57cbe39e9f22862e0b57232f64db6c9ee5c4145
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-07-31 11:51:18 -0300

    Create requisicoes.py

---
+++ src/requisicoes.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+'''
+Criado em 04/2020
+@Autor: Paulo https://github.com/alpdias
+'''
+
+def pegarUrls(mes, ano):
+    
+    """
+    Obtém as url's dos dias da agenda tributária
+    :param mes: Mês de referência
+    param ano: Ano de referência
+    return: Retorna um dicionário com os dias e a url dos dias
+    """
+    
+    url = f'https://receita.economia.gov.br/acesso-rapido/agenda-tributaria/agenda-tributaria-{ano}/agenda-tributaria-{mes}-{ano}/agenda-tributaria-{mes}-{ano}'
+
+    cabeçalho = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+
+    requisiçao = requests.get(url, headers=cabeçalho)
+
+    soup = BeautifulSoup(requisiçao.text, 'html.parser')
+
+    corpo = soup.find('div', {'id': 'parent-fieldname-text'})
+
+    elementos = corpo.find('ul')
+
+    links =  elementos.findAll('a', href=True)
+
+    dicio = {} 
+
+    for a in links:
+
+        caminho = a['href']
+        dia = a.text.strip()
+        dicio[dia] = f'{caminho}'
+
+    return dicio
+    
+    
