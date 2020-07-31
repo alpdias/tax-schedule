b6d271ad88345d8125dd5d391b9c883779fceefb
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-07-31 11:54:17 -0300

    Create tratamentos.py

---
+++ src/tratamentos.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+'''
+Criado em 07/2020
+@Autor: Paulo https://github.com/alpdias
+'''
+
+def mesCalendario(mes):
+    
+    """
+    Retornar o nome do mês equivalente ao número
+    :param mes: Número do mês
+    :return: Nome do mês
+    """
+    
+    if mes == 1:
+        mes = 'janeiro'
+    elif mes == 2:
+        mes = 'fevereiro'
+    elif mes == 3:
+        mes = 'marco'
+    elif mes == 4:
+        mes = 'abril'
+    elif mes == 5:
+        mes = 'maio'
+    elif mes == 6:
+        mes = 'junho'
+    elif mes == 7:
+        mes = 'julho'
+    elif mes == 8:
+        mes = 'agosto'
+    elif mes == 9:
+        mes = 'setembro'
+    elif mes == 10:
+        mes = 'outubro'
+    elif mes == 11:
+        mes = 'novembro'
+    elif mes == 12:
+        mes = 'dezembro'
+
+    return mes
+    
+    
