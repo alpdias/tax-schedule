93747b8517ec142eb0568ae7f263c788acf7f4e1
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-07-31 16:30:06 -0300

    Update arte.py

--- src/arte.py
+++ src/arte.py
@@ -12,7 +12,7 @@ from pyfiglet import Figlet
 def arteNome(nome, timeSleep=0):
 
     """
-    Cria uma arte ASCII com o nome do enviado
+    Cria uma arte ASCII com o nome enviado
     :param nome: Texto para criar a arte
     :para timeSleep: Tempo de espera (opcional)
     :return: Retorna arte ASCII
