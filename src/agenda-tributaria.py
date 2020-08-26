1e37f5191a1eab15bbd7e29172e174333a3576a1
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-08-26 14:04:26 -0300

    Update agenda-tributaria.py

--- src/agenda-tributaria.py
+++ src/agenda-tributaria.py
@@ -8,10 +8,11 @@ Criado em 04/2020
 # bibliotecas importadas
 import arte
 import requisicoes
-import tratamentos
 
 # bibliotecas python
 from datetime import date
+from time import sleep
+from pyfiglet import Figlet
 
 atual = date.today() # data atual
 mes = atual.month # mes atual
@@ -26,15 +27,65 @@ verificacaoMes = [mesProximo, mesAtual, mesAnterior] # lista com os meses para v
 menuNumero = [mesProximo, mesAtual, mesAnterior] # lista com os meses para criar o menu
 
 menuTexto = [] # lista para receber o nome dos meses
-  
-menuTexto.append(tratamentos.mesCalendario(mesProximo)) # funçao para criar o nome do mes
 
-menuTexto.append(tratamentos.mesCalendario(mesAtual))    
+def mesCalendario(mes):
+    
+    """
+    -> Retornar o nome do mes equivalente ao numero
+    :param mes: Numero do mes
+    :return: Nome do mes
+    """
+    
+    if mes == 1:
+        mes = 'janeiro'
+    elif mes == 2:
+        mes = 'fevereiro'
+    elif mes == 3:
+        mes = 'marco'
+    elif mes == 4:
+        mes = 'abril'
+    elif mes == 5:
+        mes = 'maio'
+    elif mes == 6:
+        mes = 'junho'
+    elif mes == 7:
+        mes = 'julho'
+    elif mes == 8:
+        mes = 'agosto'
+    elif mes == 9:
+        mes = 'setembro'
+    elif mes == 10:
+        mes = 'outubro'
+    elif mes == 11:
+        mes = 'novembro'
+    elif mes == 12:
+        mes = 'dezembro'
+
+    return mes
+  
+menuTexto.append(mesCalendario(mesProximo)) # funçao para criar o nome do mes
+
+menuTexto.append(mesCalendario(mesAtual))    
         
-menuTexto.append(tratamentos.mesCalendario(mesAnterior))
+menuTexto.append(mesCalendario(mesAnterior))
+
+def arteNome(nome, timeSleep=0):
+
+    """
+    -> Cria uma arte ASCII com o nome enviado
+    :param nome: Texto para criar a arte
+    :para timeSleep: Tempo de espera (opcional)
+    :return: Retorna arte ASCII
+    """
+    
+    f = Figlet(font='slant') # recebe a funçao mais a fonte a ser utilizada
+    nome = f.renderText(nome) # recebe o texto
+    
+    print(nome) # mostra o texto em forma de arte ASCII
+    sleep(timeSleep)
 
 print('')
-arte.arteNome('Agenda Tributaria', 2) # funçao com arte ASCII
+arteNome('Agenda Tributaria', 2) # funçao com arte ASCII
 print('')
 
 for i, menuTexto in enumerate(menuTexto):  # loop para criar menu
