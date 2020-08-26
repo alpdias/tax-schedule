973085df7be43b3c58a381ae12838749f23ffba3
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-08-26 14:18:04 -0300

    Update agenda-tributaria.py

--- src/agenda-tributaria.py
+++ src/agenda-tributaria.py
@@ -5,13 +5,12 @@ Criado em 04/2020
 @Autor: Paulo https://github.com/alpdias
 '''
 
-# bibliotecas importadas
-import requisicoes
-
 # bibliotecas python
-from datetime import date
-from time import sleep
+import requests
+from bs4 import BeautifulSoup
 from pyfiglet import Figlet
+from time import sleep
+from datetime import date
 
 atual = date.today() # data atual
 mes = atual.month # mes atual
@@ -62,6 +61,7 @@ def mesCalendario(mes):
 
     return mes
   
+    
 menuTexto.append(mesCalendario(mesProximo)) # funçao para criar o nome do mes
 
 menuTexto.append(mesCalendario(mesAtual))    
@@ -83,8 +83,9 @@ def arteNome(nome, timeSleep=0):
     print(nome) # mostra o texto em forma de arte ASCII
     sleep(timeSleep)
 
+    
 print('')
-arteNome('Agenda Tributaria', 2) # funçao com arte ASCII
+arteNome('Agenda Tributaria', 1) # funçao com arte ASCII
 print('')
 
 for i, menuTexto in enumerate(menuTexto):  # loop para criar menu
@@ -95,9 +96,109 @@ print('')
 mes = int(input('Mês de referência: ')) # entrada do mes escolhido
 print('')
 
+def pegarUrls(mes, ano):
+    
+    """
+    -> Obtem as url's dos dias da agenda tributaria
+    :param mes: Mes de referencia
+    :param ano: Ano de referencia
+    return: Retorna um dicionario com os dias e as url's dos eventos
+    """
+    
+    url = f'https://receita.economia.gov.br/acesso-rapido/agenda-tributaria/agenda-tributaria-{ano}/agenda-tributaria-{mes}-{ano}/agenda-tributaria-{mes}-{ano}' # url para a requisiçao no site
+
+    cabecalho = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} # cabeçalho para entrar no site simulando um usuario
+
+    requisicao = requests.get(url, headers=cabecalho) # requisiçao
+
+    soup = BeautifulSoup(requisicao.text, 'html.parser') # tratando o html
+
+    corpo = soup.find('div', {'id': 'parent-fieldname-text'}) # procurando uma 'div' dentro do html pelo id
+
+    elementos = corpo.find('ul') # recebendo a lista 'ul' dentro da 'div'
+
+    links =  elementos.findAll('a', href=True) # recebendo os elemento html com os links
+
+    dicio = {} # dicionario para adicionar o conteudo
+
+    for a in links:
+
+        caminho = a['href'] # links
+        dia = a.text.strip() # valor dentro da tag 'a'
+        dicio[dia] = f'{caminho}' # colocando dentro do dicionario
+
+    return dicio
+
+
+def itens(mes, ano):
+    
+    """
+    -> Obtem os itens da agenda tributaria a partir de uma url
+    :param mes: Mes de referencia
+    :param ano: Ano de referencia
+    return: Retorna dicionarios com os eventos da agenda tributaria
+    """
+
+    calendario = pegarUrls(mesCalendario(mes), ano) # funçao para buscar um dicionario contendo a agenda do mes e ano selecionado
+
+    listaDia = [] # lista para os dias da agenda
+
+    for k, v in calendario.items(): # laço para separar os dias em lista
+        listaDia.append(k)
+
+    for k, v in calendario.items(): # laço para separar os link's dos eventos da agenda
+
+        url = v # link's de cada dia da agenda
+
+        cabecalho = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+
+        requisicao = requests.get(url, headers=cabecalho) 
+
+        soup = BeautifulSoup(requisicao.text, 'html.parser') 
+
+        corpo = soup.find('div', {'id': 'parent-fieldname-text'}) 
+
+        elemento = corpo.findAll('tbody') # recebendo o corpo da tabela 'tbody' dentro da 'div'
+
+        #conteudo = elemento.findAll('td') # recebendo as linhas da tabela
+
+        lista = []
+
+        for itens in elemento: # laço para obter os conteudo da agenda tributaria
+            item = itens.findAll('td')
+            complemento = itens.find('strong')
+            
+            quantidade = len(item)
+            
+            if quantidade % 3 != 0:
+                pass
+            else:
+                for linhas in item:
+                    itemTexto = (linhas.text).replace('\xa0','').replace('\n','')
+                    lista.append(itemTexto)
+
+        conteudo = {} # dicionario para armazenar os dados da agenda por dia
+
+        conteudo[listaDia[0]] = lista
+        listaDia.pop(0)
+        
+        qtd = len(lista)
+        
+        while qtd > 0:
+            
+            for k, v in conteudo.items():
+                print(f'{k}; {v[0]}; {v[1]}; {v[2]};')
+                
+                del conteudo[k][0]
+                del conteudo[k][0]
+                del conteudo[k][0]
+                
+                qtd = qtd - 3 
+
+                
 if mes in verificacaoMes:
     try:
-        calendario = requisicoes.itens(mes, ano) # funçao para buscar os itens da agenda tributaria
+        calendario = itens(mes, ano) # funçao para buscar os itens da agenda tributaria
     except AttributeError:
         print('Mês de referência sem conteúdo!')
 
